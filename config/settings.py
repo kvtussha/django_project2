ca119ec3ccb2c45c472fa847d94aa3a6f33e1c1d
katyssha <118836572+katyssha@users.noreply.github.com>
2023-12-18 12:42:52 +0300

    rewrite ALLOWED_HOSTS

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure--(i07b*by^!fqq%4f0vvt5&*5e3u+b*4*ju7pr@f5u#dy2en7!
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
 
 
 # Application definition
