d9f4222ca6a385247d5843199f14e13632a15194
katyssha <118836572+katyssha@users.noreply.github.com>
2024-02-08 11:47:38 +0300

    delete allauth

--- users/urls.py
+++ users/urls.py
@@ -12,7 +12,7 @@ urlpatterns = [
     path('logout/', LogoutView.as_view(), name='logout'),
     path('register/', RegisterView.as_view(), name='register'),
     path('profile/', ProfileView.as_view(), name='profile'),
-    path('accounts/', include('allauth.urls')),
+    path('accounts/', include('django.urls')),
     path('password_reset/', PasswordResetView.as_view(), name='password_reset'),
     path('password_reset/done/', PasswordResetDoneView.as_view(), name='password_reset_done'),
     path('reset/<uidb64>/<token>/', PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
