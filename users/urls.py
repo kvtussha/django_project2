df604abd80d2e8d5ca109610c4913b86645e5ec1
katyssha <118836572+katyssha@users.noreply.github.com>
2024-02-09 11:44:59 +0300

    correcting a work this project

--- users/urls.py
+++ users/urls.py
@@ -9,7 +9,7 @@ from users.views import (RegisterView, ProfileView, PasswordResetView, PasswordR
 app_name = UsersConfig.name
 
 urlpatterns = [
-    path('', LoginView.as_view(template_name='users/login.html'), name='login'),
+    path('', LoginView.as_view(template_name='registration/login.html'), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
     path('register/', RegisterView.as_view(), name='register'),
     path('profile/', ProfileView.as_view(), name='profile'),
